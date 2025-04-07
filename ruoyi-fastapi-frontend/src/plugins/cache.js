24636090be2c52ba19b67b571ac551f6107ec449
wxr <17692295139@163.com>
2025-04-07 15:02:19 +0800

    add:note in cache.js

--- ruoyi-fastapi-frontend/src/plugins/cache.js
+++ ruoyi-fastapi-frontend/src/plugins/cache.js
@@ -1,12 +1,16 @@
 const sessionCache = {
+   // 设置会话存储中的键值对
   set (key, value) {
+     // 检查浏览器是否支持 sessionStorage
     if (!sessionStorage) {
       return
     }
     if (key != null && value != null) {
+      // 将键值对存储到 sessionStorage 中
       sessionStorage.setItem(key, value)
     }
   },
+   // 获取会话存储中指定键的值
   get (key) {
     if (!sessionStorage) {
       return null
@@ -14,13 +18,16 @@ const sessionCache = {
     if (key == null) {
       return null
     }
+    // 从 sessionStorage 中获取指定键的值
     return sessionStorage.getItem(key)
   },
   setJSON (key, jsonValue) {
     if (jsonValue != null) {
+      // 将 JSON 对象转换为字符串后调用 set 方法存储
       this.set(key, JSON.stringify(jsonValue))
     }
   },
+  // 获取会话存储中指定键的 JSON 对象
   getJSON (key) {
     const value = this.get(key)
     if (value != null) {
@@ -28,6 +35,7 @@ const sessionCache = {
     }
     return null
   },
+  // 从会话存储中移除指定键的值
   remove (key) {
     sessionStorage.removeItem(key);
   }
