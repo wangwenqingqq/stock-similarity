afff80a432943eab63cb5c3c2c72cfc01af7168d
wxr <17692295139@163.com>
2025-04-07 14:58:08 +0800

    add:get_clickhouse.py

---
+++ ruoyi-fastapi-backend/config/get_clickhouse.py
@@ -0,0 +1,17 @@
+from clickhouse_driver import Client
+from config.env import ClickHouseSettings
+
+settings = ClickHouseSettings()
+
+def get_clickhouse_client():
+    """
+    获取 ClickHouse 客户端连接
+    """
+    client = Client(
+        host=settings.ch_host,
+        port=settings.ch_port,
+        user=settings.ch_user,
+        password=settings.ch_password,
+        database=settings.ch_database
+    )
+    return client
