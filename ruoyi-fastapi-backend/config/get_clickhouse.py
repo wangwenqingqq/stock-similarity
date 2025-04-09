ab43ed349f82c5fa50f8c1fd8b04565f9174c95d
wxr <17692295139@163.com>
2025-04-09 10:12:05 +0800

    Revert "fix:get_clickhouse.py"
    This reverts commit 5a864b5d6cdd8139e43187fc089a58d84684dbc1.

--- ruoyi-fastapi-backend/config/get_clickhouse.py
+++ ruoyi-fastapi-backend/config/get_clickhouse.py
@@ -2,15 +2,16 @@ from clickhouse_driver import Client
 from config.env import ClickHouseSettings
 
 settings = ClickHouseSettings()
-
+#add
 def get_clickhouse_client():
     """
     获取 ClickHouse 客户端连接
     """
     client = Client(
-        host=settings.ck_host,
-        port=settings.ck_port,
-        user=settings.ck_username,
-        password=settings.ck_password
+        host=settings.ch_host,
+        port=settings.ch_port,
+        user=settings.ch_user,
+        password=settings.ch_password,
+        database=settings.ch_database
     )
     return client
