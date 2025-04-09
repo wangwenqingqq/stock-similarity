d5cee0d05e8dac2a89bd077a0ba2ecde1e400b14
wxr <17692295139@163.com>
2025-04-09 10:05:37 +0800

    Revert "fix:api change in stock_router.py"
    This reverts commit 644267bb713f42b08f4c8abed3243d32ba03a0f0.

--- ruoyi-fastapi-backend/module_stock/router/stock_router.py
+++ ruoyi-fastapi-backend/module_stock/router/stock_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from module_stock.service.stock_service import StockService
 #add
-stock_router = APIRouter(prefix='/api/system/stockInfo')
+stock_router = APIRouter(prefix='/api/stock')
 
 @stock_router.get('/list')
 def get_stock_list():
