9a537276f33da33ea7ad24d7c99839e125343175
wxr <17692295139@163.com>
2025-04-09 09:59:56 +0800

    add:catch exceptions in stock_service.py

--- ruoyi-fastapi-backend/module_stock/service/stock_service.py
+++ ruoyi-fastapi-backend/module_stock/service/stock_service.py
@@ -1,11 +1,15 @@
 from module_stock.dao.stock_dao import StockDao
 from module_admin.entity.vo.common_vo import CrudResponseModel
-#add
+import logging
+
+logger = logging.getLogger(__name__)
+
 class StockService:
     @classmethod
-    def get_stock_list_services(cls):
+    async def get_stock_list_services(cls):
         try:
-            stock_list = StockDao.get_stock_list()
-            return CrudResponseModel(is_success=True, message='获取股票信息成功', data=stock_list)
+            stock_list = await StockDao.get_stock_list()
+            return CrudResponseModel(is_success=True, message='获取股票信息成功', result=stock_list)
         except Exception as e:
+            logger.error(f"Exception in get_stock_list_services: {e}")
             return CrudResponseModel(is_success=False, message=f'获取股票信息失败，详细错误信息：{str(e)}')
