887317bcea8582021352ba64a864591bbe85da6e
wxr <17692295139@163.com>
2025-04-10 16:55:36 +0800

    fix:local swagger

--- ruoyi-fastapi-frontend/src/views/tool/swagger/index.vue
+++ ruoyi-fastapi-frontend/src/views/tool/swagger/index.vue
@@ -1,9 +1,9 @@
 <template>
+   <link rel="stylesheet" href="./node_modules/swagger-ui-dist/swagger-ui.css">
    <i-frame v-model:src="url"></i-frame>
 </template>
 
 <script setup>
 import iFrame from '@/components/iFrame'
-
 const url = ref(import.meta.env.VITE_APP_BASE_API + "/docs")
 </script>
