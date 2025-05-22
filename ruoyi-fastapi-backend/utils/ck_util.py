c926f6afc8243867850c93fe65bdfb013403b7fe
wxr <17692295139@163.com>
2025-05-22 14:29:10 +0800

    fix: change into database pool

--- ruoyi-fastapi-backend/utils/ck_util.py
+++ ruoyi-fastapi-backend/utils/ck_util.py
@@ -1,25 +1,51 @@
 import clickhouse_connect
-
 from config.env import ClickHouseConfig
+from queue import Queue
 
 CK_HOST = ClickHouseConfig.ck_host
 CK_PORT = ClickHouseConfig.ck_port
 CK_USER_NAME = ClickHouseConfig.ck_username
 CK_PWD = ClickHouseConfig.ck_password
-client = clickhouse_connect.get_client(host=CK_HOST, port=CK_PORT, username=CK_USER_NAME, password=CK_PWD)
 
+class ClickHousePool:
+    def __init__(self, maxsize=10):
+        self.pool = Queue(maxsize)
+        for _ in range(maxsize):
+            client = clickhouse_connect.get_client(
+                host=CK_HOST,
+                port=CK_PORT,
+                username=CK_USER_NAME,
+                password=CK_PWD
+            )
+            self.pool.put(client)
 
-# conn = connect(host=CK_HOST, port=CK_PORT, username=CK_USER_NAME, password=CK_PWD)
-# client = conn.cursor()
+    def get_client(self):
+        return self.pool.get()
+
+    def release_client(self, client):
+        self.pool.put(client)
+
+# 创建全局池
+ch_pool = ClickHousePool(maxsize=10)
 
 def query(sql: str):
-    result = client.query(sql)
-    return result
-
+    client = ch_pool.get_client()
+    try:
+        result = client.query(sql)
+        return result
+    finally:
+        ch_pool.release_client(client)
 
 def insert(db: str, table: str, data: list, column_names: list, column_types: list = None):
-    client.insert(table=table, database=db, data=data, column_names=column_names, column_type_names=column_types);
-
+    client = ch_pool.get_client()
+    try:
+        client.insert(table=table, database=db, data=data, column_names=column_names, column_type_names=column_types)
+    finally:
+        ch_pool.release_client(client)
 
 def insert_df(db: str, table: str, df, column_names: list, column_types: list = None):
-    client.insert_df(table=table, database=db, df=df, column_names=column_names, column_type_names=column_types);
+    client = ch_pool.get_client()
+    try:
+        client.insert_df(table=table, database=db, df=df, column_names=column_names, column_type_names=column_types)
+    finally:
+        ch_pool.release_client(client)
